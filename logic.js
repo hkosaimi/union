f6d9c07b705edec9f72115a58d4e467ffcef56d7
Hussain <hkosaimi@gmail.com>
2024-01-25 11:18:22 +0300

    navbar fix

--- logic.js
+++ logic.js
@@ -2,9 +2,14 @@ let hamMenu = document.getElementById("hamMenu");
 let ham1 = document.getElementById("ham1");
 let ham2 = document.getElementById("ham2");
 let ham3 = document.getElementById("ham3");
+let navLinks = document.getElementById("mobileNavLinks");
+let home = document.getElementById("home");
 
 hamMenu.onclick = () => {
   ham1.classList.toggle("ham11");
   ham2.classList.toggle("ham22");
   ham3.classList.toggle("ham33");
+
+  navLinks.classList.toggle("show");
+  home.classList.toggle("blur");
 };
